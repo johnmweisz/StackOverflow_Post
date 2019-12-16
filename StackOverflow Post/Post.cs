9b44d756577afe5ed1ed07abe45e7673f168671c
John Weisz <johnmweisz@gmail.com>
2019-12-16 12:43:34 -0800

    Add project files.

---
+++ StackOverflow Post/Post.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace StackOverflow_Post
+{
+    class Post
+    {
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private DateTime CreatedAt { get; set; }
+        private int _likes = 0;
+
+        public Post(string title, string description)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.CreatedAt = DateTime.Now;
+        }
+
+        public void Like()
+        {
+            _likes++;
+        }
+
+        public void Dislike()
+        {
+            _likes--;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Title: {this.Title} Description: {this.Description} Likes: {this._likes}");
+        }
+    }
+}
---
+++ StackOverflow Post/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow_Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newPost = new Post("Hello World", "This is a test");
+            newPost.Like();
+            newPost.Like();
+            newPost.Dislike();
+            newPost.Show();
+        }
+    }
+}
